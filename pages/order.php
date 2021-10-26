27ef8ffb542d67f69d1541deeb07d40556c0e422
Noah Winston Ng <noahzure@Noahs-MacBook-Pro.local>
2021-10-27 04:20:00 +0800

    initial commit

---
+++ pages/order.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping website</title>
+        <link rel="stylesheet" href="../css/index.css"/>
+        <link rel="stylesheet" href="../css/nav.css"/>
+        <link rel="stylesheet" href="../css/footer.css"/>
+        <link rel="stylesheet" href="../css/category.css"/>
+</head>
+<body>
+    <!-- Nav Bar -->
+    <nav>
+        <div class="nav-content">
+            <div class="logo">
+                <a href="../pages/index.php">
+                    <img src="../asset/photo/logo.png"/>
+                </a>
+            </div>
+            <div class="right-content">
+                <ul>
+                    <li><a href="menu.php">MENU</a><li>
+                    <li><a href="locations.php">LOCATIONS</a><li>
+                    <li><a href="contactus.php">CONTACT US</a><li>
+                    <li><a href="order.php"><img src="../asset/photo/button-order2.png"></a><li>
+                </ul>
+            </div>  
+        </div>
+    </nav>   
+
+    <!-- Main Body -->
+    <div class="container">
+        <header>
+        <img src="../asset/photo/order.png" style="width:100%">
+        </header>
+        <div style="text-align: center">
+            <p>Testing
+                 
+            </p>
+            <a href="payment.php"><h1>PAYMENT</h1></a>
+        </div>
+    </div> 
+
+
+    <!-- Footer -->
+    <footer class='footer'>
+        <div class="footer-content">
+            <div class="social-media">
+                    <a href="#"><img src="https://1.bp.blogspot.com/-WSdqH3gMHDk/U-xndvvQYrI/AAAAAAAABwg/9OLHbIjiTF8/s1600/facebook%2Blogo%2Bpng%2Btransparent%2Bbackground.png" alt="facebook"/></a>
+                    <a href="#"><img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-1024.png" alt="instagram"/></a>
+                    <a href="#"><img src="https://cdn.freebiesupply.com/logos/large/2x/telegram-logo-png-transparent.png" alt="telegram"/></a>
+            </div>
+        </div>
+        <div class="copyright">
+            &copy; 2021 - Pizza and Co. All Rights reserved. Address.
+        </div>
+    </footer>
+</body>
+</html>
